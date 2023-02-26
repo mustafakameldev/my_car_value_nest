cbecb1d1a8b33039e29d78ceb5ac4eae4b332979
Mustafa Kamel <37935806+mustafakameldev@users.noreply.github.com>
2023-02-26 12:05:02 +0200

    create report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  create(body: Report) {
+    const report = this.repo.create(body);
+    return this.repo.save(report);
+  }
+}
